0bd004e6d31eaadd84d5b208c0547a19bc3dcb33
heypop <hey@herenas.com>
2023-09-13 08:33:55 +0900

    코드 수정 및 osiv 옵션 해제

---
+++ src/main/java/com/heypop/projectboard/domain/type/SearchType.java
@@ -0,0 +1,9 @@
+package com.heypop.projectboard.domain.type;
+
+public enum SearchType {
+    TITLE,
+    CONTENT,
+    ID,
+    NICKNAME,
+    HASHTAG
+}
--- src/main/java/com/heypop/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/heypop/projectboard/repository/ArticleRepository.java
@@ -2,8 +2,11 @@ package com.heypop.projectboard.repository;
 
 import com.heypop.projectboard.domain.Article;
 import com.heypop.projectboard.domain.QArticle;
+import com.heypop.projectboard.domain.Article;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -14,16 +17,19 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>,
-        QuerydslBinderCustomizer<QArticle>
-{
+        QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitle(String title, Pageable pageable);
+
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
-        bindings.excludeUnlistedProperties(true); //선택적 검색 가능
-        bindings.including(root.title,root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
+
 }
